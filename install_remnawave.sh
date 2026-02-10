3cd675e2039d83973a8d4fee88eaec17d511d03a
DanteFuaran <DanteFuaran@users.noreply.github.com>
2026-02-10 09:55:23 +0100

    chore: bump script version to 2.3.3

--- install_remnawave.sh
+++ install_remnawave.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_VERSION="2.3.2"
+SCRIPT_VERSION="2.3.3"
 DIR_REMNAWAVE="/usr/local/remna-install/"
 DIR_PANEL="/opt/remnawave/"
 SCRIPT_URL="https://raw.githubusercontent.com/DanteFuaran/Remna-install/refs/heads/main/install_remnawave.sh"
